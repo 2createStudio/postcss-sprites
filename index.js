6411e924cc1bee320a1f315f1fdf5b94b98b8b62
Viktor Vasilev <viktor.2create@gmail.com>
2016-01-06 09:20:43 +0200

    Revert PR #25

--- index.js
+++ index.js
@@ -106,10 +106,10 @@ function plugin(opts) {
 */
 function getImages(css, opts) {
 	var images        = [];
+	var styleFilePath = css.source.input.file;
 
 	// Find only background & background-image declarations.
 	css.walkRules(function(rule) {
-		var styleFilePath = rule.source.input.file;
 		// The host object
 		// for each found image.
 		var image = {
