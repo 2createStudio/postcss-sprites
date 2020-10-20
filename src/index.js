643aa432bc63712c2ef412bcf7fa9451576961ed
Ivan Nikolić <niksy5@gmail.com>
2020-10-20 15:52:08 +0200

    Add support for PostCSS 8

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import postcss from 'postcss';
 import _ from 'lodash';
 import {
 	defaults,
@@ -18,34 +17,39 @@ import {
 /**
  * Plugin registration.
  */
-export default postcss.plugin('postcss-sprites', (options = {}) => {
-	return (css, result) => {
-		// Extend defaults
-		const opts = _.merge({}, defaults, options);
+const plugin = (options = {}) => {
+	return {
+		postcssPlugin: 'postcss-sprites',
+		Once: (css, { result }) => {
+			// Extend defaults
+			const opts = _.merge({}, defaults, options);
 
-		// Setup the logger
-		opts.logger = createLogger(opts.verbose);
+			// Setup the logger
+			opts.logger = createLogger(opts.verbose);
 
-		// Prepare filter & group functions
-		prepareFilterBy(opts, result);
-		prepareGroupBy(opts);
+			// Prepare filter & group functions
+			prepareFilterBy(opts, result);
+			prepareGroupBy(opts);
 
-		// Process it
-		return extractImages(css, opts, result)
-			.spread((opts, images) => applyFilterBy(opts, images))
-			.spread((opts, images) => applyGroupBy(opts, images))
-			.spread((opts, images) => setTokens(css, opts, images))
-			.spread((root, opts, images) => runSpritesmith(opts, images))
-			.spread((opts, images, spritesheets) => saveSpritesheets(opts, images, spritesheets))
-			.spread((opts, images, spritesheets) => mapSpritesheetProps(opts, images, spritesheets))
-			.spread((opts, images, spritesheets) => updateReferences(css, opts, images, spritesheets))
-			.spread((root, opts, images, spritesheets) => {
-				opts.logger(`${spritesheets.length} ${spritesheets.length > 1 ? 'spritesheets' : 'spritesheet'} generated.`);
-			})
-			.catch((err) => {
-				console.error(`postcss-sprites: An error occurred while processing files - ${err.message}`);
-				console.error(err.stack);
-				throw err;
-			});
+			// Process it
+			return extractImages(css, opts, result)
+				.spread((opts, images) => applyFilterBy(opts, images))
+				.spread((opts, images) => applyGroupBy(opts, images))
+				.spread((opts, images) => setTokens(css, opts, images))
+				.spread((root, opts, images) => runSpritesmith(opts, images))
+				.spread((opts, images, spritesheets) => saveSpritesheets(opts, images, spritesheets))
+				.spread((opts, images, spritesheets) => mapSpritesheetProps(opts, images, spritesheets))
+				.spread((opts, images, spritesheets) => updateReferences(css, opts, images, spritesheets))
+				.spread((root, opts, images, spritesheets) => {
+					opts.logger(`${spritesheets.length} ${spritesheets.length > 1 ? 'spritesheets' : 'spritesheet'} generated.`);
+				})
+				.catch((err) => {
+					console.error(`postcss-sprites: An error occurred while processing files - ${err.message}`);
+					console.error(err.stack);
+					throw err;
+				});
+		}
 	}
-});
+};
+export default plugin;
+plugin.postcss = true;
